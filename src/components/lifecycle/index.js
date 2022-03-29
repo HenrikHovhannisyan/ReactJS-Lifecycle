8bbb3043a161f0289e7f5ed8e81395d47cfb788e
Henrik <henrik.hovhannisyan.91@gmail.com>
2022-03-29 10:29:11 +0400

    componentDidMount

--- src/components/lifecycle/index.js
+++ src/components/lifecycle/index.js
@@ -1,13 +1,54 @@
 import React from "react";
 
-function Lifecycle (props) {
-    fetch(props.link)
-  .then(response => response.json())
-  .then(json => console.log(json))
+class Lifecycle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    }
+    this.fetchData = this.fetchData.bind(this)
+  }
+  
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(response => response.json())
+    .then(data => this.setState({data}))
+  }
 
-  return (
-    <></>
-  );
+   fetchData(url) {
+    fetch(`https://jsonplaceholder.typicode.com/${url}`)
+    .then(response => response.json())
+    .then(data => this.setState({data}))
+  }
+
+
+  render() {
+  
+    return (
+      <div style={{textAlign: 'center'}}>
+      {console.log(this.state.data)}
+        <h2>Title</h2>
+        <button onClick={() => this.fetchData('todos')}>Todos</button>
+        <button onClick={() => this.fetchData('posts')}>Posts</button>
+        <hr></hr>
+        {
+          this.state.data && this.state.data.map((data) => {
+            if(data.body) {
+              return (
+                <>
+                  <p>{data.title}</p>
+                  <strong>{data.body}</strong>
+                  <hr></hr>
+                </>
+                )
+            } else {
+              return (<p>{data.title}</p>)
+            }
+          })
+        }
+      </div>
+    );
+  };
 }
 
 export default Lifecycle;
